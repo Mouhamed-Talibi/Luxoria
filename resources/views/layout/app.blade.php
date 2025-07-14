7d99766164738dec5c7c17d2f766f815e6bd04ea
Mouhamed-Talibi <mouha22talibi@gmail.com>
2025-07-14 20:30:40 +0100

    Starting navbar

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{-- csrf-token --}}
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+    {{-- logo --}}
+    <link rel="icon" href="{{ asset('assets/luxoria.png') }}" type="image/png">
+
     <title>
-        Luxoria | @yield('title')
+        Luxoria - @yield('title')
     </title>
 
     {{-- bootstrap css --}}
@@ -23,18 +29,29 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-    {{-- navbar --}}
-    @include('partials.navbar')
+    {{-- loader --}}
+    <div id="loader" class="loader-container">
+        <div class="loader"></div>
+        <p>Loading Luxoria, Please Wait..</p>
+    </div>
 
-    {{-- main content --}}
-    <main>
-        @yield('content')
-    </main>
+    {{-- content site --}}
+    <div id="content">
+        {{-- navbar --}}
+        @include('partials.navbar')
 
-    {{-- footer --}}
-    @include('partials.footer')
+        {{-- main content --}}
+        <main>
+            @yield('content')
+        </main>
+
+        {{-- footer --}}
+        @include('partials.footer')
+    </div>
 
     {{-- bootstrap js --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+    {{-- loader --}}
+    <script src="{{ asset('js/loader.js') }}"></script>
 </body>
 </html>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,1 +1,43 @@
-This is the navbar
+<!-- start nav -->
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container">
+        <!-- logo -->
+        <a class="navbar-brand fs-4" href="#">
+            <img src="{{ asset('assets/luxoria.png') }}" alt="" style="height:70px; width:70px;">
+        </a>
+        <!-- toggle btn -->
+        <button class="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- sidebar -->
+        <div class="sidebar offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <!-- sidebar header -->
+            <div class="offcanvas-header text-dark borde-bottom">
+                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Bootstrap Navbar-1</h5>
+                <button type="button" class="btn-close btn-close-dark shadow-none" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <!-- sidebar body -->
+            <div class="offcanvas-body d-flex flex-column flex-lg-row p-2 p-lg-0">
+                <ul class="navbar-nav justify-content-center align-items-center flex-grow-1 pe-3">
+                    <li class="nav-item mx-2">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link" href="#">Services</a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+                <!-- login / Signup -->
+                <div class="d-flex flex-column flex-lg-row justify-content-center align-items-center gap-3">
+                    <a href="" class="text-dark">Login</a>
+                    <a href="" class="text-dark text-decoration-none px-3 py-1 rounded-4" style="background-color: #28a2c7;">Sign Up</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
